ba4ceec0a71114a951a4766b31c4aa095dd2555c
allenmatasy <allen.matasy@gmail.com>
2017-10-03 13:32:18 -0700

    initial revision

--- Python/Lab1Turtle.py
+++ Python/Lab1Turtle.py
@@ -1,8 +1,13 @@
 from turtle import *
+'''
+this draws a Daniel-san from Karate Kid.
+'''
+
 pu()
 goto(0,120)
 pd()
 circle(50)
+#head
 forward(90)
 right(90)
 forward(220)
@@ -16,6 +21,7 @@ right(90)
 pu()
 goto(90,80)
 pd()
+# Right Arm
 left(90)
 forward(90)
 left(90)
@@ -24,6 +30,7 @@ circle(10)
 pu()
 goto(-90,80)
 pd()
+# Left Arm
 left(90)
 forward(90)
 right(90)
@@ -32,13 +39,55 @@ circle(10)
 pu()
 goto(-90,-100)
 pd()
+# Left Leg
 left(90)
 forward(90)
 left(90)
 forward(90)
+# Foot
 right(90)
+forward(50)
+left(90)
 forward(30)
+left(90)
+forward(50)
+left(90)
+forward(30)
+pu()
+goto(90,-100)
+pd()
+# Right Leg
+left(180)
+forward(180)
+# Foot
+right(90)
+forward(50)
+left(90)
+forward(30)
+left(90)
+forward(50)
+left(90)
+forward(30)
+# End stick figure
+# begin letter 'K'
 
+pu()
+goto(-250,300)
+pd()
+left(180)
+forward(50)
+left(180)
+forward(25)
+right(144)
+forward(35)
+pu()
 
+goto(-250,280)
+pd()
+left(80)
+forward(35)
+
+# Begin letter 'A'
+pu()
 
 done()
