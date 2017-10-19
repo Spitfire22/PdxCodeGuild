6ad80310522ca7cbce79056fbeb687880b5c88f8
allenmatasy <allen.matasy@gmail.com>
2017-10-19 16:11:45 -0700

    Updated New changes 10-19

--- Python/Lab27-ContactList.py
+++ Python/Lab27-ContactList.py
@@ -31,44 +31,102 @@ for line in lines:
 ## Implement CRUD
 ## Create a record - ask a new contact and for each attribute
 
+
 def create_user():
-    print('Creating a new user - Please answer the following questions')
-    name = input('What is your name? ')
-    fav_color = input('What is your favorite color? ')
-    fav_fruit = input('What is your favorite fruit? ')
-    new = contacts.append({'name': name, 'favorite color': fav_color, 'favorite fruit': fav_fruit})
+    print('\nCreating a new user - Please answer the following questions\n')
+    name_input = input('What is your name? \n\t')
+    fav_color = input('What is your favorite color? \n\t')
+    fav_fruit = input('What is your favorite fruit? \n\t')
+    new_contact = {'name': name_input, 'favorite color': fav_color, 'favorite fruit': fav_fruit}
+    new = contacts.append(new_contact)
+    #retrieve = [name for name in contacts if name['name'] == name_input]
+    print('\nYou have added: ', new_contact)
 
-def only_names():
+def print_names():
     name = [name['name'] for name in contacts]
-    print(name)
-    #format this list into a string and order it alphabetically, then print it
-
+    print(sorted(name))
 
 def retrieve_user():
-    print('Retrieving a users information - What user do you want to view?')
-    only_names()
-    input('Which user do you want to edit? ')
-    pass
+    print('\nRetrieving a users information - Here are your users: ')
+    print_names()
+    prompt = input('\nWhich user do you want to view? \n\t')
+    contact = find_user(prompt)
+    retrieve = [name for name in contacts if name['name'] == prompt]
+    if contact is None:
+        print(prompt, 'is a contact not found in the database, check or update the spelling or create a new contact')
+        return
+    print(retrieve)
+    # retrieve = [name for name in contacts if name ['name']==prompt]
+    # print(retrieve)
+    while True:
+        command = input('\nDo you want to view another user? Yes / No? \n\t').lower()
+        if command in ['no', 'n', 'nah', 'naw']:
+            return
+        elif command in ['yes', 'y', 'yeah', 'yea', 'yeah dude']:
+            retrieve_user()
+        else:
+            pass
+
+
+def find_user(name):
+    for contact in contacts:
+        if contact['name'] == name:
+            return contact
+    return None
 
 def update_user():
-    print('Updating a users information - What user do you want to update?')
-    # we will need the list of names again here to select.
+    print('Updating users menu... 3... 2... 1... Loaded!\n')
+    print_names()
+    prompt = input('\nWhich user do you want to edit? \n\t')
+    #e = [name for name in contacts if name['name'] == prompt][0]
+    contact = find_user(prompt)
+    retrieve = [name for name in contacts if name['name'] == prompt]
+    if contact is None:
+        print(prompt, 'is a contact not found in the database, check or update the spelling or create a new contact')
+        return
+    print(retrieve)
+    while True:
+        command_update = input('\nWhat do you want to edit? \nName, Color, or Fruit? Or you can type \'exit\' or \'done\' to return to main menu. \n').lower()
+        if command_update in ['done', 'quit', 'exit']:
+            return
+        elif command_update in ['name', 'names']:
+            name_input = input('\nYou want to change the name to? \n\t')
+            # print(name_input)
+            contact['name'] = name_input
+            print('\nYou have changed their name: ', contact,'\n')
+        elif command_update in ['color', 'colour']:
+            color_input = input('\nWhat is their new favorite colour? \n\t')
+            contact['favorite color'] = color_input
+            print('\nColor has been changed: ', contact,'\n')
+        elif command_update in ['fruit', 'fruity']:
+            fruit_input = input('\nTastebuds change, what is their new favorite fruit? \n\t')
+            contact['favorite fruit'] = fruit_input
+            print('\nNew tasty changes!', contact,'\n')
+        else:
+            break
 
 def delete_user():
-    print('Deleting a user from the list - which user do you want to delete?')
-    #selecting a user to delete
-    while True:
-        confirmation = input('Are you sure? Yes or No')
-        if confirmation in ['yes', 'Yes', 'YES', 'y']:
-            pass
-            #delete the user use .pop
-        else:
-             return
+    print('\nDeleting a user from the list\n')
+    print_names()
+    prompt = input('\nWhich user in the list do you want to delete? \n\t')
+    contact = find_user(prompt)
+    # retrieve = [name for name in contacts if name['name'] == prompt]
+    if contact is None:
+        print(prompt, 'is a contact not found in the database, check or update the spelling or create a new contact')
+        return
+    # print(retrieve)
+    confirmation = input('Are you sure? Yes or No\n\t').lower()
+    if confirmation in ['no', 'n']:
+        return
+        #delete the user use .pop
+    elif confirmation in ['yes', 'y']:
+        contacts.remove(contact)
+        print(prompt, 'was removed!')
 
 
 def ask():
     while True:
-        command = input('|Contact List| Type a Command: Create, Retrieve, Update, Delete, or Exit ?').lower()
+        command = input('\n|Contact List| Type a Command: Create, Retrieve, Update, Delete, or Exit ?\n\t').lower()
         if command in ['done', 'quit', 'exit']:
             break
         elif command == 'retrieve':
@@ -83,7 +141,21 @@ def ask():
             ask()
 
 ask()
-print(contacts)
+
+
+### Version 3 ---------------------------------------
+### On exit - place 'the_keys' back at the beginning
+### join the keys with ','
+### and join the strings with a new line.
+### Load back into the file.
+
+
+
+
+#
+# with open('contacts_2.csv', 'w') as file1:
+#     file1.write('\n'.join(contacts))
+# print(contacts)
 
 
 #
@@ -92,14 +164,13 @@ print(contacts)
 # print(contacts)
 
 
-
-
-
-
-
-
-
-
+# I was thinking about making more functions to include a possible option
+# while in the retrieve function to switch to update user.
+# def update_prompt():
+#     print('\nUpdating a users information - What user do you want to update?')
+#     only_names()
+#     input('Which user do you want to edit? ')
+#     pass
 
 
 #contacts = {
--- Python/Testing.py
+++ Python/Testing.py
@@ -1,31 +1,4 @@
-'''
-
-
-This is a file I use to text out ideas to see if they will work.
-
-
-'''
-
-
-f = open('hammer.txt', 'r')
-# g = open('hammer2.txt', 'w')
-
-contents = f.read().lower()
-for char in ";,(){}.:[]'\"#+=*&^%$@!\n/-_><0123456789":
-    contents = contents.replace(char, "")
-word_list = contents.split(" ")
-print(word_list)
 
 
 
-# g.write(contents)
-
-f.close()
-# g.close()
-
-
-
-# with open('hammer.txt', 'r') as repub:
-#     contents = repub.read()
-#
-# print(contents)
+w
