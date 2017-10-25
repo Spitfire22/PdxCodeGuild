97eda336923b49bb1217100de70f22c3bceb5e33
allenmatasy <allen.matasy@gmail.com>
2017-10-25 15:56:57 -0700

    Updated New changes 10-25 version 2

--- Python/lab29-ROADTRIP.py
+++ Python/lab29-ROADTRIP.py
@@ -19,18 +19,28 @@ city_to_accessible_cities = {
 ## Print out all of the cities connected to that via a hop
 ## Then print out all of the cities that can be reached through two hops
 
+def hop(city):
+    return city_to_accessible_cities[city]
+    #return a list of all the cities in 1 hop
+
+def hop2(city):
+    one_hop = hop(city)
+    two_hops = set() # set is like a list but you can only have unique elements
+    for one_hop_city in one_hop:
+        two_hop_cities = hop(one_hop_city)
+        two_hops |= two_hop_cities
+    return two_hops
+
+
 def roadtrip():
     starting = []
     start = input('Enter starting city:\nBoston | New York | Albany | Portland | Philadelphia\n= ')
-    hop1 = (city_to_accessible_cities[start])
+    hop1 = hop(start)
     print('Cities reached in 1 Hop:', hop1)
-
-    hop2 = 0
-
-
+    hop_two = hop2(start)
     # hop2 = (city_to_accessible_cities[hop1]) Doesn't work
     # achieve, use the dictionary of the start to access those and print those cities
     # dictionaries.
-    print('From', start, 'through', hop2, 'you can reach')
+    print('From', start, 'through', hop_two, 'you can reach')
 
 roadtrip()
--- Python/lab32-RainData.py
+++ Python/lab32-RainData.py
@@ -5,6 +5,8 @@ Local Portland Oregon Rain Data - rain data != gray data
 '''
 
 import datetime
+import math
+
 #
 # datatime = '3-JUN-2014' # example
 #
@@ -19,14 +21,46 @@ import datetime
 # print(lines)
 
 def load_file(rain_file):
-    header = []
     rain_data = []
     with open(rain_file, 'r') as file:
         lines = file.read().split('\n')
-        for i in range(12,len(lines)):
-            rain_values = lines[i].split('  ')
+        for i in range(11,len(lines)-1):
+            rain_values = lines[i].split()
+            rain_values[0] = datetime.datetime.strptime(rain_values[0], '%d-%b-%Y')
+            for j in range(1, len(rain_values)):
+                if rain_values[j] != '-':
+                    rain_values[j] = int(rain_values[j])
+            rain_data.append(rain_values)
+    return rain_data
+
+
+# def average(rain_data):
+#     # using the rain_data[1] which is daily rainfall. Calc average.
+#     avg = 0
+#     for num in rain_data[1]:
+#         avg += num
+#     return avg/len(rain_data)
+#
+# def variance(nums, average):
+#     total = 0
+#     for num in nums:
+#         diff = num - average
+#         total += diff*diff
+#     return total/len(nums)
+
+#av = average( insert nums )
+#var = variance( insert nums, av)
+#std = math.sqrt(var)
 
 
 
+
+# index 1 is the total of the day of each list
 
 rain_file = 'raindata.txt'
+rain_data = load_file(rain_file)
+# rain_avg = average(rain_data)
+print(rain_data)
+# print(rain_avg)
+
+
